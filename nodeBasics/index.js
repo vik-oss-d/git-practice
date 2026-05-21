880a1686c45a6ff9821ec64d2ff8759f4f5c4901
victoria.bezugla <vvv37783@gmail.com>
2026-05-21 16:41:49 +0300

    new lesson3

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -5,3 +5,7 @@ console.log('Hello, World!');
 console.log(chalk.blue('Welcome to JavaScript!'));
 
 console.log(chalk.red('This is a red message.'));
+
+console.log(chalk.green('This is a green message.'));
+
+console.log(chalk.yellow('This is a yellow message.'));
