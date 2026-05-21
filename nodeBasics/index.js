ed40d94dd67977e1b10594e5163b3b3cf4c4f5c1
victoria.bezugla <vvv37783@gmail.com>
2026-05-21 16:36:38 +0300

    lesson3

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log('Hello, World!');
+
+console.log(chalk.blue('Welcome to JavaScript!'));
+
+console.log(chalk.red('This is a red message.'));
---
+++ nodeBasics/variables.js
@@ -0,0 +1,7 @@
+var age=10
+let name="John"
+const city = "New York"
+
+console.log(age)
+console.log(name)
+console.log(city)
