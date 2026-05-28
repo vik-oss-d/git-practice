61f0b1711bbe54036b91b9ddac1b71e928565954
victoria.bezugla <vvv37783@gmail.com>
2026-05-28 21:28:48 +0300

    lesson3

--- nodeBasics/variables.js
+++ nodeBasics/variables.js
@@ -1,7 +1,40 @@
-var age=10
-let name="John"
-const city = "New York"
+// let age = 10;
+// let name = "John";
+// const city = "New York";
+// let userName = "Alice";
+    
 
-console.log(age)
-console.log(name)
-console.log(city)
+// console.log(age)
+// console.log(name)
+// console.log(city)
+// console.log(userName)
+
+// age = 11;
+// console.log(age)
+
+
+// Numbers
+// let age = 10.5;
+// console.log(typeof age); // Output: number
+
+// String
+// let name = "John";
+// console.log(typeof name); // Output: string
+
+// Boolean
+// let isRaining = true;
+// console.log(typeof isRaining); // Output: boolean
+
+// NaN
+// let result = 'Hello'/2;
+// console.log(result); // Output: 5
+
+//Infinity
+//let result = 1/0;
+//console.log(result); // Output: Infinity
+
+//BigInt
+//let number = 1234567890123456789012345678901234567890n;
+//console.log(number); // Output: 1234567890123456789012345678901234567890n
+// let age = 10;
+// console.log(Math.cbrt)
