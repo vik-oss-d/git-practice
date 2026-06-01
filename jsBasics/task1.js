604bf36251e10080ddd49e16552081bfbc12e96d
victoria.bezugla <vvv37783@gmail.com>
2026-06-01 09:33:13 +0300

    homework4

---
+++ jsBasics/task1.js
@@ -0,0 +1,15 @@
+// Завдання 1: Оголошення змінних для примітивних типів
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. 
+// Присвойте їм значення та виведіть кожну змінну на консоль.
+
+let text = "Hello!"
+let number = 3;
+let isActive = true;
+let emptyValue = null;
+let notDefined = undefined;
+
+console.log(text);
+console.log(number);
+console.log(isActive);
+console.log(emptyValue);
+console.log(notDefined);    
